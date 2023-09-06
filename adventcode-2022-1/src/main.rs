8e64a968beaa4777dde3f50c77bc6fa6a26a7d75
Elwin <elwin@mmisys.net>
2023-09-06 14:11:11 -0700

    update

--- adventcode-2022-1/src/main.rs
+++ adventcode-2022-1/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Seek, SeekFrom};
+use std::io::{ BufRead, BufReader };
 use std::collections::BinaryHeap;
 
 
@@ -77,8 +77,8 @@ fn find_top_three(file: &File) -> i32 {
         if let Some(max) = max_heap.pop() {
             res += max;
         } else {
-            // Handle the case where there are fewer than three elements in the heap
-            break;
+            println!("Error");
+            return -1;
         }
     }
 
